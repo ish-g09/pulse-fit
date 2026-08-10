86961070b2bb2f51ecabd4eb5e3df360e47d849c
ish-g09 <ig.valiente09@gmail.com>
2026-08-10 22:53:56 +0530

    chore: initialize spring boot project with maven dependencies

---
+++ src/main/java/com/pulsefit/pulsefit/PulseFitApplication.java
@@ -0,0 +1,13 @@
+package com.pulsefit.pulsefit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PulseFitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pulsefit/pulsefit/PulseFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulsefit.pulsefit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseFitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
