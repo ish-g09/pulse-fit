0c4a42ff5bd3ec909da269b7d1d8321a1ca5a7d4
ish-g09 <ig.valiente09@gmail.com>
2026-08-11 01:21:05 +0530

    feat(async): enable asynchronous processing in Spring Boot

--- src/main/java/com/pulsefit/pulsefit/PulseFitApplication.java
+++ src/main/java/com/pulsefit/pulsefit/PulseFitApplication.java
@@ -2,8 +2,10 @@ package com.pulsefit.pulsefit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class PulseFitApplication {
 
     public static void main(String[] args) {
